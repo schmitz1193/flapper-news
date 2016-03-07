2e74c97de220e3d8da1e6fd63ad22b9355fbf127
schmitz1193 <schmitz1193@gmail.com>
2016-03-07 09:08:27 -0600

    factory working

--- app.js
+++ app.js
@@ -24,7 +24,17 @@ app.factory('posts', [function(){
   var o = {
     posts: []
   };
-  return o;
+  // return o;
+// }]);
+  return {
+    getPost: function() {
+      return o.posts;
+    },
+    setPost: function(newPosts) {
+      o.posts = newPosts;
+      console.log("factory posts ", o.posts);
+    }
+  }
 }]);
 
 app.controller('MainCtrl', [
@@ -32,10 +42,18 @@ app.controller('MainCtrl', [
 'posts',
 function($scope, posts){
   $scope.test = 'Hello world!';
+  console.log("post at Mainctrl ", posts);
   $scope.posts = posts.posts;
-  // $scope.posts = posts.posts;
   $scope.posts = [
-    {title: 'post 1', upvotes: 5},
+    {title: 'post 1',
+     upvotes: 5,
+     link: "schmitz1193@github.io",
+     upvotes: 0,
+     comments: [
+        {author: 'Hillary', body: 'Women should rule!', upvotes: 0},
+        {author: 'Bill', body: 'What she said!', upvotes: 0},
+        {author: 'me', body: 'FYI not May id', upvotes: 0}
+               ]},
     {title: 'post 2', upvotes: 2},
     {title: 'post 3', upvotes: 15},
     {title: 'post 4', upvotes: 9},
@@ -43,6 +61,7 @@ function($scope, posts){
   ];
 
   $scope.addPost = function(){
+    console.log("$scope.title ", $scope.title);
     if(!$scope.title || $scope.title === '') { return; }
     $scope.posts.push({
       title: $scope.title,
@@ -54,15 +73,22 @@ function($scope, posts){
         {author: 'me', body: 'what is happening?', upvotes: 0}
       ]
     });
-    $scope.title = '';
-    $scope.link = '';
+      console.log("addPOst ", $scope.posts);
+    // $scope.title = '';
+    // $scope.link = '';
   };
 
   $scope.incrementUpvotes = function(post) {
     post.upvotes += 1;
   };
 
+  console.log("comments? ", $scope.posts);
+
+  //send the posts to the factory to store the data
+  posts.setPost($scope.posts);
+
  }  //end function $scope
+
 
 ]);  //end app controller
 
@@ -72,14 +98,18 @@ app.controller('PostsCtrl', [
 '$stateParams',
 'posts',
 function($scope, $stateParams, posts){
+  $scope.title="HELP!";
+  posts.posts = posts.getPost();
+  console.log("in postctrl ", posts.posts);
   $scope.post = posts.posts[$stateParams.id];
   $scope.addComment = function(){
-    if($scope.body === '') { return; }
+    // if($scope.body === '') { return; }
+    if(!$scope.body || $scope.body === '') { return; }
     $scope.post.comments.push({
       body: $scope.body,
       author: 'user',
       upvotes: 0
   });
-  $scope.body = '';
+  // $scope.body = '';
 };
 }]);
